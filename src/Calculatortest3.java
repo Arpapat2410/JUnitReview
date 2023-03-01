ae6c35aec960825e33fe7444ad46560765868c96
Arpapat2410 <120714445+Arpapat2410@users.noreply.github.com>
2023-03-01 15:20:38 +0700

    Initial commit

---
+++ src/Calculatortest3.java
@@ -0,0 +1,30 @@
+
+import junit.framework.TestCase;
+
+public class Calculatortest3 extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	
+	public void testAddNumberPositive() {
+		Calculator c1 = new Calculator();
+		assertEquals(14,c1.addNumber(5,9));
+		assertEquals(57,c1.addNumber(5,52));
+	}
+	
+	
+	public void testAddNumberNagative() {
+		Calculator c1 = new Calculator();
+		assertEquals(-4,c1.addNumber(5,-9));
+		assertEquals(-107,c1.addNumber(-55,-52));
+	}
+	
+	
+
+}
